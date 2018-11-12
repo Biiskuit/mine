3004cdd52661e625b7c6bfeb1bf208f8eafb33ed
Biiskuit <35196333+Biiskuit@users.noreply.github.com>
2018-11-12 14:43:53 +0100

    Versuch 2

--- github versuch/github versuch/Program.cs
+++ github versuch/github versuch/Program.cs
@@ -11,6 +11,7 @@ namespace github_versuch
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Versuch2");
             Console.ReadLine();
         }
     }
